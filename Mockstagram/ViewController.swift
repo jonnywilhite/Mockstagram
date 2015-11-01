77ae695c3d29ce710b6180f07275448a24991b97
Jonathan Wilhite <jonnygwilhite@gmail.com>
2015-11-01 11:08:37 -0800

    Blank slate, imported SQL.swift

---
+++ Mockstagram/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mockstagram
+//
+//  Created by Jonathan Wilhite on 10/31/15.
+//  Copyright © 2015 Jonathan Wilhite. All rights reserved.
+//
+
+import UIKit
+import SQLite
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+use_frameworks!
+
+target 'Mockstagram' do
+
+pod 'SQLite.swift',
+	git: 'https://github.com/stephencelis/SQLite.swift.git'
+
+end
+
+target 'MockstagramTests' do
+
+end
+
+target 'MockstagramUITests' do
+
+end
+
